8ae16ba1111558196447411f27c885ecb120b41d
mikeydimaria <michael.dimaria1@marist.edu>
2017-02-21 18:49:12 -0500

    Lab 3

--- labs/3/Driver_Lab3.java
+++ labs/3/Driver_Lab3.java
@@ -1,31 +1,34 @@
+/**
+   * file: Driver_Lab3.java
+   * author: Michael DiMaria
+   * course: CMPT 220
+   * assignment: Lab 3 Driver
+   * due date: Febuary 21, 2017
+   * version: 1.0
+   * 
+   * Kattis Different Distances
+   * 
+   */
 import java.util.Scanner;
-import java.lang.*;
+
 public class Driver_Lab3 {
-public static void main (String[] args) {
-		
-  double x1;
-  double y1;
-  double x2;
-  double y2; 
-  double p; // coordinates of two points
-  double distance; // distance between the points
-  int count = 0;
-  Scanner input = new Scanner(System.in);
 
-    x1 = input.nextDouble();
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-    y1 = input.nextDouble();
+    while (true) {
+      String line = sc.nextLine();
+      if ("0".equals(line)) {break;}
 
-    x2 = input.nextDouble();
-
-    y2 = input.nextDouble();
-
-    p = input.nextDouble();
-    
-    distance = Math.pow(Math.pow(Math.abs(x1-x2), p) + Math.pow(Math.abs(y1-y2), p), 1/p);
-    
-    
-    System.out.println(distance);
-  
-  } 
+      String[] parts = line.split(" ");
+      double x1 = Double.parseDouble(parts[0]);
+      double y1 = Double.parseDouble(parts[1]);
+      double x2 = Double.parseDouble(parts[2]);
+      double y2 = Double.parseDouble(parts[3]);
+      double p = Double.parseDouble(parts[4]);
+      double distance = Math.pow(Math.pow(Math.abs(x1 - x2), p) + 
+        Math.pow(Math.abs(y1 - y2), p), 1 / p);
+      System.out.printf("%6f\n", distance);
+    }
+  }
 }
