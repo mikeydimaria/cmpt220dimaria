8bba9a08beeef89df1b51ddbf94cb7b3e2f0cf99
mikeydimaria <michael.dimaria1@marist.edu>
2017-04-23 11:28:44 -0400

    Lab 7

---
+++ labs/7/Lab7_1010.java
@@ -0,0 +1,12 @@
+public class Lab7_1010 {
+  public static void main(String[] args) {
+	Queue queue = new Queue();
+      for (int i = 1; i <= 20; i++) {
+		queue.enqueue(i);
+	 }
+	  while (!queue.empty()) {
+		System.out.print(queue.dequeue() + " ");
+	  }
+	  System.out.println();
+	}
+}
---
+++ labs/7/Square.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+public class Square {
+  public static void main(String[] args) {
+	BigInteger i = new BigInteger(Long.MAX_VALUE + ""); 
+	BigInteger end = i.add(new BigInteger("10"));
+	  for (i = new BigInteger(Long.MAX_VALUE + ""); 
+		i.compareTo(end) <= 0; 
+		i = i.add(new BigInteger("1"))) {
+		System.out.println(i.multiply(i)); 
+    }		
+  }
+}
